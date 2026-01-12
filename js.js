5f710a359c9c5df33595b9bcc4fe4371b7c6961c
guypeleg2010-wq <guypeleg2010@gmail.com>
2026-01-12 17:25:12 +0200

    TASK#2 Creat Project

---
+++ js.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+function personalname() {
+    let name = prompt('enter your name', "jony sins")
+    console.log(name)
+}
+
+function name() {
+    let isname = confirm("are yourr realy" + name)
+}
