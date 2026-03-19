403a8f37cc77b6baef8f6f26f3f462c16f819f7e
guypeleg2010-wq <guypeleg2010@gmail.com>
2026-03-19 11:42:45 +0200

    Task4. Contact form

--- js.js
+++ js.js
@@ -1,9 +1,62 @@
-// JavaScript source code
-function personalname() {
-    let name = prompt('enter your name', "jony sins")
-    console.log(name)
-}
 
-function name() {
-    let isname = confirm("are yourr realy" + name)
+
+function verifyuser() {
+
+        let flag = true
+    let username = document.querySelector('#username').value
+    let email = document.querySelector('#email').value
+    let telephone = document.querySelector('#telephone').value
+    let id = document.querySelector('#id').value
+    let age = Number(document.querySelector('#age').value)
+
+
+    if (age < 18 || age > 120)
+    {
+        alert('גילך לא מתאים!')
+        flag = false
+        return
+    }
+
+    if (id.length != 9)
+    {
+        alert('תעודת זהות שגויה!')
+        flag = false
+        return
+
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email))
+    {
+        alert("כתובת אימייל לא חוקית");
+        flag = false
+        return
+    }
+
+    if (telephone.length != 10)
+    {
+        alert('מספר טלפון לא מזוהה!')
+        flag = false
+        return
+    }
+
+    if (username.length < 5)
+    {
+        alert('אורך שם משתמש חייב להיות גדול מ-5!')
+        flag = false
+        return
+    }
+
+    if (flag)
+    {
+        alert('ההרשמה בוצעה בהצלחה!')
+        console.log({
+            username: username,
+            email: email,
+            telephone: telephone,
+            id: id,
+            age: age
+        });
+};
 }
+    
